4157eee25c8b31cfd755d5692faff71c14be61f3
Joe Cross <joe.mcross@gmail.com>
2023-09-26 21:35:53 -0400

    first impl nginx-rtmp-auth

---
+++ nginx-rtmp-auth/README.md
@@ -0,0 +1,99 @@
+# micro-auth
+
+## What is this
+
+A very small, **very simple** rust-based http server that can be used as the
+authentication endpoint for publishing rtmp streams through the
+[nginx-rtmp-module](https://github.com/arut/nginx-rtmp-module).
+
+When the process starts, it loads an access key from the env variable
+`MA_ACCESS_KEY` or generates a secure random one if the variable isn't present.
+
+On each incoming request, it checks the query string for a known key
+(default: "key", configurable with `MA_QUERYSTRING_KEY`) and compares that
+query string value against the known access key.
+
+It returns 204 on success, 403 on wrong access key, and 400 on missing access
+key.
+
+This is designed to be a drop-in auth mechanism for `micro-rtmp-nginx` when
+you don't need anything more secure than a static key for publishing/playing
+rtmp streams.
+
+## How do I use this in docker?
+
+Run this image (TODO: not connected to dockerhub yet)
+```bash
+$ docker run --rm -d -p 5000:5000 micro-rtmp-auth:1
+9840a2d81b2f7b9dd5fd9d60cbe890f37655a839518b1ed91c497bdf020281a9
+```
+
+Let's get the access key:
+```bash
+$ docker logs 984
+generating random access key (set your own with MA_ACCESS_KEY=)
+socket: 0.0.0.0:5000
+access_key: 11119691
+querystring_key: key
+```
+
+And verify that works:
+```bash
+$ curl -i localhost:5000?key=11119691
+HTTP/1.1 204 No Content
+date: Mon, 01 Jan 1980 00:00:01 GMT
+```
+
+## How do I configure this
+
+There are a few environment variables:
+
+* `MA_ACCESS_KEY` (default: `""``) -- the access key that callers must provide.
+  If this is empty or unset, a secure random value will be generated.
+* `MA_SOCKET` (default: `"0.0.0.0:5000"`) -- ip and port to bind to.
+* `MA_QUERYSTRING_KEY` (default: `"key"`) -- the query string param name
+  that the caller must provide the access key at
+
+
+We can easily build and test this without docker.  We'll use a
+generated access key and look for the query string param name `"secret"`:
+
+```
+$ cargo build
+
+$ MA_QUERYSTRING_KEY="secret" ./target/debug/micro-auth
+generating random access key (set your own with MA_ACCESS_KEY=)
+socket: 0.0.0.0:5000
+access_key: 1e0b81d5
+querystring_key: secret
+```
+
+In another terminal, let's test the correct key:
+```
+$ curl -i localhost:5000?secret=1e0b81d5
+HTTP/1.1 204 No Content
+date: Mon, 01 Jan 1980 00:00:01 GMT
+```
+
+The wrong query string key returns a 400:
+```
+$ curl -i localhost:5000?wrong_key=1e0b81d5
+HTTP/1.1 400 Bad Request
+content-length: 0
+date: Mon, 01 Jan 1980 00:00:01 GMT
+```
+
+The wrong value returns a 403:
+```
+$ curl -i localhost:5000?secret=hunter2
+HTTP/1.1 401 Unauthorized
+content-length: 0
+date: Mon, 01 Jan 1980 00:00:01 GMT
+```
+
+Here's the corresponding output from the server:
+```
+allow: correct access key
+deny: missing access key
+deny: incorrect access key
+```
---
+++ nginx-rtmp-auth/src/main.rs
@@ -0,0 +1,135 @@
+use core::fmt::Write;
+use hyper::{
+    server::conn::AddrStream,
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server, StatusCode,
+};
+use nanorand::{BufferedRng, ChaCha20, Rng};
+use std::{convert::Infallible, env, net::SocketAddr, sync::Arc};
+use subtle::ConstantTimeEq;
+use url;
+
+const ACCESS_KEY_ENV: &'static str = "MA_ACCESS_KEY";
+const DEFAULT_ACCESS_KEY_LENGTH_BYTES: usize = 4;
+
+const SOCKET_ENV: &'static str = "MA_SOCKET";
+const DEFAULT_SOCKET: &'static str = "0.0.0.0:5000";
+
+const QUERY_STRING_KEY_ENV: &'static str = "MA_QUERYSTRING_KEY";
+const DEFAULT_QUERY_STRING_KEY: &'static str = "key";
+
+struct ServiceConfig {
+    access_key: String,
+    listen_socket: SocketAddr,
+    qs_key: String,
+}
+
+macro_rules! expect {
+    ( $e: expr, $msg: expr ) => {
+        match $e {
+            Ok(x) => x,
+            Err(_) => {
+                die!($msg);
+            }
+        }
+    };
+}
+
+macro_rules! die {
+    ( $arg: expr ) => {
+        println!("error: {}", $arg);
+        std::process::exit(1);
+    };
+}
+
+#[tokio::main]
+async fn main() {
+    let svc_cfg = Arc::new(load_service_config());
+    let make_svc = make_service_fn(|_: &AddrStream| {
+        let svc_cfg = svc_cfg.clone();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req: Request<Body>| {
+                let svc_cfg = svc_cfg.clone();
+                async move { handle(req, &svc_cfg).await }
+            }))
+        }
+    });
+    let server = Server::bind(&svc_cfg.listen_socket).serve(make_svc);
+    println!("socket: {}", svc_cfg.listen_socket);
+    println!("access_key: {}", svc_cfg.access_key);
+    println!("querystring_key: {}", svc_cfg.qs_key);
+    match server.await {
+        Ok(_) => {
+            println!("shutting down");
+        }
+        Err(e) => {
+            die!(format!("exception: {e}"));
+        }
+    }
+}
+
+async fn handle(req: Request<Body>, cfg: &ServiceConfig) -> Result<Response<Body>, Infallible> {
+    let claimed_key = find_one_qs_value(req, &cfg.qs_key);
+    let status = match claimed_key {
+        Some(claimed_key) => {
+            let actual_key = &cfg.access_key;
+            if claimed_key.as_bytes().ct_eq(actual_key.as_bytes()).into() {
+                println!("allow: correct access key");
+                StatusCode::NO_CONTENT
+            } else {
+                println!("deny: incorrect access key");
+                StatusCode::UNAUTHORIZED
+            }
+        }
+        None => {
+            println!("deny: missing access key");
+            StatusCode::BAD_REQUEST
+        }
+    };
+    Ok(expect!(
+        Response::builder().status(status).body(Body::empty()),
+        "failed to build body"
+    ))
+}
+
+fn find_one_qs_value(req: Request<Body>, key: &String) -> Option<String> {
+    let qs = req.uri().query().unwrap_or("");
+    let mut it = url::form_urlencoded::parse(qs.as_bytes());
+    it.find_map(|(k, v)| match k == *key {
+        true => Some(v.trim().to_string()),
+        false => None,
+    })
+}
+
+fn load_service_config() -> ServiceConfig {
+    let mut access_key: String = env::var(ACCESS_KEY_ENV).unwrap_or("".into()).trim().into();
+    if access_key.len() == 0 {
+        println!("generating random access key (set your own with {ACCESS_KEY_ENV}=)");
+        access_key = generate_access_key();
+    }
+
+    let listen_str = env::var(SOCKET_ENV).unwrap_or(DEFAULT_SOCKET.into());
+    let listen_socket = expect!(
+        listen_str.parse::<SocketAddr>(),
+        format!("malformed {SOCKET_ENV}='{listen_str}'")
+    );
+
+    let qs_key = env::var(QUERY_STRING_KEY_ENV).unwrap_or(DEFAULT_QUERY_STRING_KEY.into());
+
+    ServiceConfig {
+        access_key,
+        listen_socket,
+        qs_key,
+    }
+}
+
+fn generate_access_key() -> String {
+    let mut raw = [0u8; DEFAULT_ACCESS_KEY_LENGTH_BYTES];
+    let mut rng = BufferedRng::new(ChaCha20::new());
+    rng.fill(&mut raw);
+    let mut key = String::with_capacity(2 * raw.len());
+    for byte in raw {
+        expect!(write!(key, "{:02x}", byte), "failed to generate access key");
+    }
+    key
+}
